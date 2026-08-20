d80ef52a83d3efa7e97a1f1cf9997fcb35476884
DC PRACMATIC <dcpracmatic@gmail.com>
2026-08-20 00:25:30 -0600

    Update spmm.c

--- src/spmm.c
+++ src/spmm.c
@@ -1,6 +1,6 @@
+
 #include <stdlib.h>
 #include <string.h>
-#include <immintrin.h>
 #include <omp.h>
 #include "spmm.h"
 
@@ -23,88 +23,13 @@ void spmm_hbag_core(const CSRMatrix *A, const float *B, float *C, int n, int k) 
         int row_start = A->row_ptr[i];
         int row_end = A->row_ptr[i + 1];
         float *c_row = &C[i * k];
-
-        int c = 0;
-        #if defined(__AVX2__)
-        // Process in chunks of 32 floats (128 bytes, 4 YMM registers)
-        for (; c <= k - 32; c += 32) {
-            __m256 acc0 = _mm256_setzero_ps();
-            __m256 acc1 = _mm256_setzero_ps();
-            __m256 acc2 = _mm256_setzero_ps();
-            __m256 acc3 = _mm256_setzero_ps();
-
-            for (int j = row_start; j < row_end; j++) {
-                float val = A->values[j];
-                int col = A->col_idx[j];
-                const float *b_ptr = &B[col * k + c];
-                __m256 v_val = _mm256_set1_ps(val);
-
-                __m256 b0 = _mm256_loadu_ps(b_ptr);
-                __m256 b1 = _mm256_loadu_ps(b_ptr + 8);
-                __m256 b2 = _mm256_loadu_ps(b_ptr + 16);
-                __m256 b3 = _mm256_loadu_ps(b_ptr + 24);
-
-                acc0 = _mm256_fmadd_ps(v_val, b0, acc0);
-                acc1 = _mm256_fmadd_ps(v_val, b1, acc1);
-                acc2 = _mm256_fmadd_ps(v_val, b2, acc2);
-                acc3 = _mm256_fmadd_ps(v_val, b3, acc3);
-            }
-
-            _mm256_storeu_ps(&c_row[c], acc0);
-            _mm256_storeu_ps(&c_row[c + 8], acc1);
-            _mm256_storeu_ps(&c_row[c + 16], acc2);
-            _mm256_storeu_ps(&c_row[c + 24], acc3);
-        }
-
-        // Process remaining 16 floats if any
-        for (; c <= k - 16; c += 16) {
-            __m256 acc0 = _mm256_setzero_ps();
-            __m256 acc1 = _mm256_setzero_ps();
-
-            for (int j = row_start; j < row_end; j++) {
-                float val = A->values[j];
-                int col = A->col_idx[j];
-                const float *b_ptr = &B[col * k + c];
-                __m256 v_val = _mm256_set1_ps(val);
-
-                __m256 b0 = _mm256_loadu_ps(b_ptr);
-                __m256 b1 = _mm256_loadu_ps(b_ptr + 8);
-
-                acc0 = _mm256_fmadd_ps(v_val, b0, acc0);
-                acc1 = _mm256_fmadd_ps(v_val, b1, acc1);
-            }
-
-            _mm256_storeu_ps(&c_row[c], acc0);
-            _mm256_storeu_ps(&c_row[c + 8], acc1);
-        }
-
-        // Process remaining 8 floats if any
-        for (; c <= k - 8; c += 8) {
-            __m256 acc0 = _mm256_setzero_ps();
-
-            for (int j = row_start; j < row_end; j++) {
-                float val = A->values[j];
-                int col = A->col_idx[j];
-                const float *b_ptr = &B[col * k + c];
-                __m256 v_val = _mm256_set1_ps(val);
-
-                __m256 b0 = _mm256_loadu_ps(b_ptr);
-
-                acc0 = _mm256_fmadd_ps(v_val, b0, acc0);
-            }
-
-            _mm256_storeu_ps(&c_row[c], acc0);
-        }
-        #endif
-
-        // Scalar residue for c < k
-        if (c < k) {
-            for (int cc = c; cc < k; cc++) {
-                float sum = 0.0f;
-                for (int j = row_start; j < row_end; j++) {
-                    sum += A->values[j] * B[A->col_idx[j] * k + cc];
-                }
-                c_row[cc] = sum;
+        memset(c_row, 0, k * sizeof(float));
+        for (int j = row_start; j < row_end; j++) {
+            float val = A->values[j];
+            int col = A->col_idx[j];
+            const float *b_row = &B[col * k];
+            for (int c = 0; c < k; c++) {
+                c_row[c] += val * b_row[c];
             }
         }
     }
@@ -135,84 +60,14 @@ void spmm_hbag_core_omp(const CSRMatrix *A, const float *B, float *C, int n, int
         int row_start = A->row_ptr[i];
         int row_end = A->row_ptr[i + 1];
         float *c_row = &C[i * k];
+        memset(c_row, 0, k * sizeof(float));
 
-        int c = 0;
-        #if defined(__AVX2__)
-        for (; c <= k - 32; c += 32) {
-            __m256 acc0 = _mm256_setzero_ps();
-            __m256 acc1 = _mm256_setzero_ps();
-            __m256 acc2 = _mm256_setzero_ps();
-            __m256 acc3 = _mm256_setzero_ps();
-
-            for (int j = row_start; j < row_end; j++) {
-                float val = A->values[j];
-                int col = A->col_idx[j];
-                const float *b_ptr = &B[col * k + c];
-                __m256 v_val = _mm256_set1_ps(val);
-
-                __m256 b0 = _mm256_loadu_ps(b_ptr);
-                __m256 b1 = _mm256_loadu_ps(b_ptr + 8);
-                __m256 b2 = _mm256_loadu_ps(b_ptr + 16);
-                __m256 b3 = _mm256_loadu_ps(b_ptr + 24);
-
-                acc0 = _mm256_fmadd_ps(v_val, b0, acc0);
-                acc1 = _mm256_fmadd_ps(v_val, b1, acc1);
-                acc2 = _mm256_fmadd_ps(v_val, b2, acc2);
-                acc3 = _mm256_fmadd_ps(v_val, b3, acc3);
-            }
-
-            _mm256_storeu_ps(&c_row[c], acc0);
-            _mm256_storeu_ps(&c_row[c + 8], acc1);
-            _mm256_storeu_ps(&c_row[c + 16], acc2);
-            _mm256_storeu_ps(&c_row[c + 24], acc3);
-        }
-
-        for (; c <= k - 16; c += 16) {
-            __m256 acc0 = _mm256_setzero_ps();
-            __m256 acc1 = _mm256_setzero_ps();
-
-            for (int j = row_start; j < row_end; j++) {
-                float val = A->values[j];
-                int col = A->col_idx[j];
-                const float *b_ptr = &B[col * k + c];
-                __m256 v_val = _mm256_set1_ps(val);
-
-                __m256 b0 = _mm256_loadu_ps(b_ptr);
-                __m256 b1 = _mm256_loadu_ps(b_ptr + 8);
-
-                acc0 = _mm256_fmadd_ps(v_val, b0, acc0);
-                acc1 = _mm256_fmadd_ps(v_val, b1, acc1);
-            }
-
-            _mm256_storeu_ps(&c_row[c], acc0);
-            _mm256_storeu_ps(&c_row[c + 8], acc1);
-        }
-
-        for (; c <= k - 8; c += 8) {
-            __m256 acc0 = _mm256_setzero_ps();
-
-            for (int j = row_start; j < row_end; j++) {
-                float val = A->values[j];
-                int col = A->col_idx[j];
-                const float *b_ptr = &B[col * k + c];
-                __m256 v_val = _mm256_set1_ps(val);
-
-                __m256 b0 = _mm256_loadu_ps(b_ptr);
-
-                acc0 = _mm256_fmadd_ps(v_val, b0, acc0);
-            }
-
-            _mm256_storeu_ps(&c_row[c], acc0);
-        }
-        #endif
-
-        if (c < k) {
-            for (int cc = c; cc < k; cc++) {
-                float sum = 0.0f;
-                for (int j = row_start; j < row_end; j++) {
-                    sum += A->values[j] * B[A->col_idx[j] * k + cc];
-                }
-                c_row[cc] = sum;
+        for (int j = row_start; j < row_end; j++) {
+            float val = A->values[j];
+            int col = A->col_idx[j];
+            const float *b_row = &B[col * k];
+            for (int c = 0; c < k; c++) {
+                c_row[c] += val * b_row[c];
             }
         }
     }
